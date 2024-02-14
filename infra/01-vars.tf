ea708bfa0f13bc0004ad294704c2d292517f350f
Dante Raul Paniagua <taten14@gmail.com>
2024-02-14 16:12:57 -0300

    first commit

--- infra/01-vars.tf
+++ infra/01-vars.tf
@@ -10,10 +10,10 @@ variable "sso_region" {
 
 variable "aws_secret_access_key" {
     description = "AWS Access key"
-    default = "oR+HCfez1ycP6OnRbnggSWgo5jVBLxeLLxlbBcfS"
+    default = ${{ secrets.AWS_SECRET_ACCES_KEY }}
 }
 
 variable "aws_access_key_id"     {
     description = "AWS Access key id"
-    default = "AKIARJBDP2DTQPGV4RV2"
+    default = ${{ secrets.AWS_ACCES_KEY_ID }}
 }

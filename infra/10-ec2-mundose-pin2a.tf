0bee59832c987fe0ad842e8527e6b7cf01d1e551
Dante Raul Paniagua <taten14@gmail.com>
2024-02-14 17:32:06 -0300

    Primer Commit

--- infra/10-ec2-mundose-pin2a.tf
+++ infra/10-ec2-mundose-pin2a.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "mundose-pin2" {
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.main.id]
   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
-  key_name                    = aws_key_pair.kp.public_key.name
+  key_name                    = aws_key_pair.kp.key_name
 
   tags = {
     Name = "ec2-mundose-pin2"

5d76ea8a2202f4fbc5729a8cda271d583cd8d011
Dante Raul Paniagua <taten14@gmail.com>
2024-02-14 16:56:49 -0300

    Primer Commit

--- infra/09-remote-access.tf
+++ infra/09-remote-access.tf
@@ -1,13 +1,15 @@
-variable "key_name" {}
-
-resource "tls_private_key" "example" {
+resource "tls_private_key" "pk" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "aws_key_pair" "generated_key" {
-  key_name   = var.key_name
-  public_key = tls_private_key.example.public_key_openssh
+resource "aws_key_pair" "kp" {
+  key_name   = "myKey"       # Create "myKey" to AWS!!
+  public_key = tls_private_key.pk.public_key_openssh
+
+  provisioner "local-exec" { # Create "myKey.pem" to your computer!!
+    command = "echo '${tls_private_key.pk.private_key_pem}' > ./myKey.pem"
+  }
 }
 
 #resource "aws_key_pair" "devkey" {
